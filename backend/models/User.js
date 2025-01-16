58ba62678d7d4bce1aad3deb1b90bfbe2778da28
Mehmet Samet Gok <msametgok@hotmail.com>
2025-01-15 19:28:32 -0500

    user model is created

---
+++ backend/models/User.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs'
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        default: ''
+    }
+}, {
+    timestamps: true
+});
+
+//hash password before saving
+userSchema.pre('save', async function (next){
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
